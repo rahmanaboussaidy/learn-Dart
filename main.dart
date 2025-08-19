f31a4154c7687682c173015af3e42db0cb604535
Rahmani Said <rahmanaboussaidy09@gmail.com>
2025-08-19 14:26:50 +0300

    another

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'user.dart';
+
+void main() {
+  var user1 = User("nyanda", 23);
+  var user2 = User("nyanda", 23);
+
+  print(user1);
+  print(user2);
+
+ print( user1 == user2);
+}
---
+++ user.dart
@@ -0,0 +1,7 @@
+class User {
+  final name;
+  final age;
+
+  User(this.name, this.age);
+  
+}
