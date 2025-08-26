e73646dc4d1a58f8b76d0258ab487b58507e0e39
Rahmani Said <rahmanaboussaidy09@gmail.com>
2025-08-26 19:02:00 +0300

    clean flutter project

--- BusinessCard.dart
+++ BusinessCard.dart
@@ -36,40 +36,56 @@ class BusinessCard extends StatelessWidget {
       // backgroundColor: Color(0xFFfacc15),
       appBar: AppBar(title: Text("Business Card")),
 
-      body: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          color: Color(0xFFfacc15),
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 10,
-              spreadRadius: 10,
-              color: Colors.limeAccent,
-            ),
-            BoxShadow(blurRadius: 10, spreadRadius: 10, color: Colors.black26),
-            BoxShadow(
-              blurRadius: 10,
-              spreadRadius: 10,
-              color: Colors.blueAccent,
-            ),
-            BoxShadow(blurRadius: 10, spreadRadius: 10, color: Colors.white38),
-          ],
-        ),
-        padding: EdgeInsets.all(30),
-        margin: EdgeInsets.all(10),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Flexible(
-              flex: 1,
-              child: Text(
-                "Name: Rashidi Abdallah",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Color(0xFFfacc15),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 10,
+                spreadRadius: 10,
+                color: Colors.limeAccent,
               ),
-            ),
-            Flexible(flex: 1, child: Text("Job Tittle: MERN Stack Developer")),
-            Flexible(flex: 1, child: Text("Email: rashidabdallah4@gmail.com")),
-          ],
+              BoxShadow(
+                blurRadius: 10,
+                spreadRadius: 10,
+                color: Colors.black26,
+              ),
+              BoxShadow(
+                blurRadius: 10,
+                spreadRadius: 10,
+                color: Colors.blueAccent,
+              ),
+              BoxShadow(
+                blurRadius: 10,
+                spreadRadius: 10,
+                color: Colors.white38,
+              ),
+            ],
+          ),
+          padding: EdgeInsets.all(30),
+          margin: EdgeInsets.all(10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Flexible(
+                flex: 1,
+                child: Text(
+                  "Name: Rashidi Abdallah",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Text("Job Tittle: MERN Stack Developer"),
+              ),
+              Flexible(
+                flex: 1,
+                child: Text("Email: rashidabdallah4@gmail.com"),
+              ),
+            ],
+          ),
         ),
       ),
     );
