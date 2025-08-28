6c00ec282900e00533f4afa556350b2e1a59dc6e
Rahmani Said <rahmanaboussaidy09@gmail.com>
2025-08-28 17:10:15 +0300

    clean flutter project

---
+++ buttons.dart
@@ -0,0 +1,37 @@
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("FAB Pressed!");
+          },
+          child: Icon(Icons.add),
+        ),
+
+        // ElevatedButton(
+        //   onPressed: () {
+        //     Text("Hello");
+        //   },
+        //   child: Text("Click"),
+        // ),
+
+        // //Iconic button
+        // IconButton(
+        //   icon: Icon(Icons.thumb_up),
+        //   onPressed: () {
+        //     print("Liked!");
+        //   },
+        // ),
+
+        // //Outlined button
+        // OutlinedButton(
+        //   onPressed: () {
+        //     print("Outlined Button Pressed!");
+        //   },
+        //   child: Text("Register"),
+        // ),
+
+        //Textbutton
+        // TextButton(
+        //   onPressed: () {
+        //     print("Text Button Pressed!");
+        //   },
+        //   child: Text("Login"),
+        // ),
